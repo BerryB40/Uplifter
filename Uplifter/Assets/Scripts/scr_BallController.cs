91e7802d968fb160bdf2e8134a31beb9f07ad132
Jake Garrett <jake-garrett@hotmail.com>
2016-06-21 14:23:10 +1000

    First Commit
    Test scene, basic movement, possible placeholder sprites.

---
+++ Uplifter/Assets/Scripts/scr_BallController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_BallController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public int jump;
+    public int moveSpeed;
+    public bool canJump;
+
+	void Start () 
+    {
+ 
+	}
+	
+	
+	void Update () 
+    {
+        canJump = true;
+
+        if (canJump == true) 
+        {
+            BallMovement();
+        }
+       
+	}
+
+    void BallMovement() 
+    {
+        if (canJump = true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            Debug.Log("GroundCheck");
+            canJump = true;
+        }
+    }
+}
