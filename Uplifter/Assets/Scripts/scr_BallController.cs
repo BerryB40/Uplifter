5953b226fd08da2beba0cad40022fead460afac7
Jake Garrett <jake-garrett@hotmail.com>
2016-06-22 19:51:40 +1000

    Ball Physics
    yeah, ball physics and stuff

--- Uplifter/Assets/Scripts/scr_BallController.cs
+++ Uplifter/Assets/Scripts/scr_BallController.cs
@@ -4,35 +4,37 @@ using System.Collections;
 public class scr_BallController : MonoBehaviour {
 
     public Rigidbody2D rb;
-    public int jump;
-    public int moveSpeed;
-    public bool canJump;
+    public float jump;
+    public float moveSpeed;
+    public float rotSpeed;
+    public bool isGrounded;
+    public PhysicMaterial bounce;
+    public CircleCollider2D circleCol;
+ 
 
+
+  
 	void Start () 
     {
- 
+        bounce = GetComponent<PhysicMaterial>();
+        circleCol = GetComponent<CircleCollider2D>();
 	}
 	
 	
 	void Update () 
     {
-        canJump = true;
-
-        if (canJump == true) 
-        {
-            BallMovement();
-        }
-       
+           BallMovement();
+  
 	}
 
-    void BallMovement() 
-    {
-        if (canJump = true && Input.GetKeyDown(KeyCode.Space))
+    void BallMovement()
+    { //------------------------- Keyboard Movement------------------
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jump, ForceMode2D.Force);
         }
 
-        if (Input.GetKey(KeyCode.A)) 
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * moveSpeed * Time.deltaTime;
         }
@@ -41,14 +43,66 @@ public class scr_BallController : MonoBehaviour {
         {
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
         }
-    }
 
-    void OnCollisionEnter2D(Collision2D other) 
-    {
-        if (other.gameObject.tag == "ground")
+        //------------------------------- Xbox Movement ------------------------
+
+         if (isGrounded == true && Input.GetButtonDown("A")) 
+         {
+             rb.AddForce(Vector2.up * jump, ForceMode2D.Force);
+         }
+
+
+        if (Input.GetAxis("LeftStickX") > 0)
         {
-            Debug.Log("GroundCheck");
-            canJump = true;
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward * -rotSpeed);
         }
+
+        if (Input.GetAxis("LeftStickX") < 0)
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.forward * rotSpeed);
+        }
+        //--------------------- To stop the ball bouncing ----------------------
+
+        if (Input.GetAxis("LeftStickY") > 0.8)
+        { 
+			Debug.Log ("Down");
+            circleCol.sharedMaterial = null; 
+        }
+
+        if (Input.GetAxis("LeftStickY") == 0)
+        {
+            PhysicsMaterial2D bouncy = new PhysicsMaterial2D();
+            bouncy.bounciness = .3f;
+            circleCol.sharedMaterial = bouncy;
+        }
+     
+       
+    
+
+
+        }
+    
+
+    void OnCollisionStay2D(Collision2D coll) 
+    {
+        isGrounded = true;
+
+
+        if (coll.gameObject.tag == "wall") 
+        {
+            isGrounded = false;
+        }
+
+       /* if (coll.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+        */
+    }
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        isGrounded = false;
     }
 }
---
+++ Uplifter/Assets/Scripts/scr_CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_CameraController : MonoBehaviour
+{
+    public Transform target;            // What is the camera following
+    public float smoothing = 5f;        //Camera Follow speed
+
+    public Vector3 offset;              // Camera offset of player
+
+    void Start()
+    {
+      
+    }
+
+    void FixedUpdate()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+
+    }
+}
